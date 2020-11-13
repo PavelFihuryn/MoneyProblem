8c1b9bf2373a00cee0965a8e7c6e49dfd3fa5c36
PavelFihuryn <belarus19@tut.by>
2020-11-13 14:38:04 +0300

    Добавил запись результатов в файл

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 class Men:
     """Создание участника мероприятия """
+
     def __init__(self):
         self.name = ''  # имя
         self.sum = 0  # сколько внес изначально
@@ -14,11 +15,13 @@ class Men:
 
 class Company:
     """Создание группы участников. Вычисление кто-кому должен."""
+
     def __init__(self):
         self.list = []  # список компании
         self.num = 0  # количество участников
         self.all_sum = 0  # общая сумма затрат
         self.get_company()  # ввод участников
+        self.result_file = open("result.txt", 'w')
 
     def get_company(self):  # Создаем группу участников и вносим предварительные их затраты
         try:
@@ -56,16 +59,22 @@ class Company:
             if men.sum != 0:  # Если предварительно тратил
                 men.debt_to_me = men.sum - self.part_of_sum  # то ему должны за вычетом солидарной ответственности
             if men.debt_to_me == 0:  # Если предварительно не тратил
-                men.debt_i = self.part_of_sum   # то должен солидарную
+                men.debt_i = self.part_of_sum  # то должен солидарную
             elif men.debt_to_me < 0:  # Если тебе должны отрицательное значение
                 men.debt_i = self.part_of_sum - men.sum  # то ты должен солидарную за вычетом предварительно сданной
-                men.debt_to_me = 0   # и, соответственно, тебе не должны
+                men.debt_to_me = 0  # и, соответственно, тебе не должны
+
+    def print_and_write(self, text):
+        self.result_file.writelines(text)
+        self.result_file.writelines('\n')
+        print(text)
 
     def started(self):  # Для начала выведем сводно кто сколько сдавал и сколько он или ему должны
         for i in self.list:
-            print(f'{i.name}: {i.sum}, ему должны: {format(i.debt_to_me, ".2f")}, он должен {format(i.debt_i, ".2f")}')
-        print('\nОбщая смета: ', self.all_sum)
-        print('Солидарная ответственнось: ', format(self.part_of_sum, ".2f"), end='\n\n')
+            self.print_and_write(f'{i.name} потратил: {i.sum}, ему должны:'
+                                 f' {format(i.debt_to_me, ".2f")}, он должен {format(i.debt_i, ".2f")}')
+        self.print_and_write(f'\nОбщая смета: {self.all_sum}')
+        self.print_and_write(f'Солидарная ответственнось: {format(self.part_of_sum, ".2f")}\n')
 
     def who_should(self):
         for men in self.list:  # Проверяем каждого участника
@@ -74,10 +83,12 @@ class Company:
                     if to_me.debt_i > 0:  # должен ли он, если да то спрашиваем
                         if men.debt_to_me > to_me.debt_i:  # мне должны больше чем он должен
                             men.debt_to_me = men.debt_to_me - to_me.debt_i  # забираем что у него есть
-                            print(f'{to_me.name} отдает {men.name}: {format(to_me.debt_i, ".2f")} рублей')
+                            self.print_and_write(f'{to_me.name} отдает {men.name}: '
+                                                 f'{format(to_me.debt_i, ".2f")} рублей')
                             to_me.debt_i = 0
-                        if men.debt_to_me <= to_me.debt_i and men.debt_to_me != 0: # мне должны меньше или столько же чем он должен
-                            print(f'{to_me.name} отдает {men.name}: {format(men.debt_to_me, ".2f")} рублей')
+                        if men.debt_to_me <= to_me.debt_i and men.debt_to_me != 0:  # мне должны меньше или столько же чем он должен
+                            self.print_and_write(f'{to_me.name} отдает {men.name}: '
+                                                 f'{format(men.debt_to_me, ".2f")} рублей')
                             to_me.debt_i = to_me.debt_i - men.debt_to_me  # забираем часть, которую должны мне
                             men.debt_to_me = 0
 
@@ -85,6 +96,7 @@ class Company:
         self.debt()
         self.started()
         self.who_should()
+        self.result_file.close()
 
 
 if __name__ == "__main__":
